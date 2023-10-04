910d6c0dbdb221952193833cfa31bd94d6b9f2f5
evitali <emanuele.vitali@csc.fi>
2023-10-04 14:57:00 +0300

    fixed solution for multi array section fortran

--- OpenMP_gpu/data/multi_array_section/solutions/ans_multi_array_section.F90
+++ OpenMP_gpu/data/multi_array_section/solutions/ans_multi_array_section.F90
@@ -8,13 +8,13 @@ program main
    !! one thread executes the loop
    !! TODO 1:  map all of y as tofrom, and only approriately needed section of x
 
-   !omp target map(to:x(1:n/4),x[n/2+1:n]) map(tofrom:y)
+   !$omp target map(to:x(1:n/4),x(n/2+1:n)) map(tofrom:y)
     do i=1,    n/4; x(i)=1; enddo
     do i=n/2+1,n  ; x(i)=1; enddo
 
     do i=1,    n/4; y(i)=a*x(i)+y(i); enddo
     do i=n/2+1,n  ; y(i)=a*x(i)+y(i); enddo
-   !omp end target
+   !$omp end target
 
    do i=1,    n/4; if(y(i) /= 3) stop("y != 3"); enddo
    do i=n/2+1,n  ; if(y(i) /= 3) stop("y != 3"); enddo
