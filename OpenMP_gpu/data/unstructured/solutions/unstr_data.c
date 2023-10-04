94666d2f6c6f36667fd54ce941764c43d81c289c
evitali <emanuele.vitali@csc.fi>
2023-10-04 15:03:44 +0300

    add solution, fix solution

--- OpenMP_gpu/data/unstructured/solutions/unstr_data.c
+++ OpenMP_gpu/data/unstructured/solutions/unstr_data.c
@@ -12,11 +12,14 @@ void init_data(Vec *A, int n)
   A->N = n;
   A->data = (double *)malloc(n*sizeof(double));
   // TODO 1:  insert a a target enter data construct here
+  #pragma omp target enter data map(alloc:A, A->data[0:n]   )
+  #pragma omp target update to (A->N)
 }
 
 void free_data(Vec *A)
 {
   // TODO 2:  insert a a target exit data construct here
+  #pragma omp target exit data  map(delete:A->data[0:A->N])
   A->N = 0;
   free(A->data);
   A->data = NULL;
@@ -29,14 +32,19 @@ int main(){
   init_data(&A,npts);
  
   // TODO 3:  offload with a target construct (no clauses)
+  #pragma omp target
  
   for(int i=0; i<A.N; i++){
     A.data[i]=1.0f; 
    }
  
   // TODO 4:  use an update construct to get back from device
- 
-  printf("A.data[0] = %f\n",A.data[0]);
+  #pragma omp target update from(A.data[0:A.N])
+
+
+
+  for (int i=0; i<A.N; ++i) 
+  printf("A.data[%d] = %f\n",i,A.data[i]);
  
   free_data(&A);
  
