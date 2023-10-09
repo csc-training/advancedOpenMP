07fd1872552755a7fdf07291c193496d80500800
Kent Milfeld <milfeld@tacc.utexas.edu>
2023-10-09 12:01:19 +0300

    Took out answers in apis example

--- OpenMP_cpu/adv_intro/1_env_apis/env_apis.c
+++ OpenMP_cpu/adv_intro/1_env_apis/env_apis.c
@@ -6,15 +6,11 @@ int main(){
 // TODO 3: display the "ENV" and affinity
    // omp_display_<TODO>;
    // omp_display_<TODO>;
-   //omp_display_env(1);
-   //omp_display_affinity(NULL);
-// omp_display_se
 
 // TODO 1: get the thread number and number of threads
    #pragma omp parallel
    {   
     // printf( " %d out of %d\n", omp_get_<TODO>, omp_get_<TODO> );
-      printf( " %d out of %d\n", omp_get_thread_num(), omp_get_num_threads() );
    }   
 
 }
