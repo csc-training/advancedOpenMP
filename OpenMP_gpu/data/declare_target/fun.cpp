00d3ad84a72539498ab2adff59c63deff12140bc
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2024-09-09 14:42:15 +0300

    Removing old material

--- OpenMP_gpu/data/declare_target/fun.cpp
+++
@@ -1,5 +0,0 @@
-// TODO 2:  Add begin/end declare target directive
-//          around the global and function declaration
-static int a=2.0f;
-void fun(int i, float *x, float *y)
-        { y[i]=a*x[i]+y[i]; }
